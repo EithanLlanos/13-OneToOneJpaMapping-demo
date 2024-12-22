e27c1353486a570483674357742894e0c4b80a86
Rubick <eithanllanosb@gmail.com>
2024-12-22 12:22:35 -0500

    Set up simple connection to db

--- hibernateAdvancedMappings/src/main/java/com/springCourse/hibernateAdvancedMappings/HibernateAdvancedMappingsApplication.java
+++ hibernateAdvancedMappings/src/main/java/com/springCourse/hibernateAdvancedMappings/HibernateAdvancedMappingsApplication.java
@@ -1,13 +1,22 @@
 package com.springCourse.hibernateAdvancedMappings;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateAdvancedMappingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateAdvancedMappingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateAdvancedMappingsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
 
 }
